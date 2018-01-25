016e52d4a82666552164f84f1a24ce9e1c509cdb
Rishit Shah <rishits85@gmail.com>
2018-01-25 11:05:04 -0600

    Update tree-height-actual.py

--- Data_Structures/Week 1/tree_height/tree-height-actual.py
+++ Data_Structures/Week 1/tree_height/tree-height-actual.py
@@ -8,47 +8,7 @@ threading.stack_size(2 ** 25)  # new thread will get stack of such size
 
 
 class TreeHeight:
-    """Computes height of a given tree.
-    Height of a (rooted) tree is the maximum depth of a node, or the maximum
-    distance from a leaf to the root.
-    Samples:
-    >>> tree = TreeHeight()
-    >>> tree.n = 5
-    >>> tree.parent = [4, -1, 4, 1, 1]
-    >>> tree.compute_height()
-    3
-    >>> # Explanation:
-    >>> # The input means that there are 5 nodes with numbers from 0 to 4,
-    >>> # node 0 is a child of node 4, node 1 is the root, node 2 is a child of
-    >>> # node 4, node 3 is a child of node 1 and node 4 is a child of node 1.
-    >>> #
-    >>> #       root
-    >>> #         1
-    >>> #        /|
-    >>> #       3 4
-    >>> #      /  |
-    >>> #     0   2
-    >>> #
-    >>> tree = TreeHeight()
-    >>> tree.n = 5
-    >>> tree.parent = [-1, 0, 4, 0, 3]
-    >>> tree.compute_height()
-    4
-    >>> # Explanation:
-    >>> # The input means that there are 5 nodes with numbers from 0 to 4,
-    >>> # node 0 is the root, node 1 is a child of node 0, node 2 is a child of
-    >>> # node 4, node 3 is a child of node 0 and node 4 is a child of node 3.
-    >>> #
-    >>> #       root
-    >>> #         1
-    >>> #        /|
-    >>> #       1 3
-    >>> #         |
-    >>> #         4
-    >>> #         |
-    >>> #         2
-    """
-
+   
     def __init__(self):
         self.n = 0
         self.parent = []
